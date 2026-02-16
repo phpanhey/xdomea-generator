f0c47973e7447a7f034a323215dc9c511ec966e7
philipp panhey <philipp.panhey@gmail.com>
2026-02-16 15:57:39 +0100

    feat: now generating xdomea

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 type Node struct {
@@ -16,30 +19,86 @@ type Node struct {
 func main() {
 	http.HandleFunc("/generatexdomea", Generatexdomea)
 	fmt.Println("Server running on :8080")
+	fmt.Println("/generatexdomea endpoint is exposed")
 	http.ListenAndServe(":8080", nil)
+
+	/*
+	   		filesMap := readFiles()
+	   		jsondata := `[
+	   	   {
+	   	        "type": "akte",
+	   	        "label": "Akte 1",
+	   	        "children": [
+	   	            {
+	   	                "type": "vorgang",
+	   	                "label": "Vorgang 1",
+	   	                "children": [
+	   	                    {
+	   	                        "type": "untervorgang",
+	   	                        "label": "Untervorgang 1",
+	   	                        "children": [
+	   	                            {
+	   	                                "type": "dokument",
+	   	                                "label": "Dokument 1"
+	   	                            }
+	   	                        ]
+	   	                    }
+	   	                ]
+	   	            }
+	   	        ]
+	   	    }
+
+	   ]`
+
+	   	var nodes []Node
+	   	if err := json.Unmarshal([]byte(jsondata), &nodes); err != nil {
+	   		panic("json could not be parsed")
+	   	}
+
+	   	xml := filesMap["base_prefix"]
+
+	   	for _, node := range nodes {
+	   		xml += nodeToXML(node, filesMap)
+	   	}
+
+	   	xml += filesMap["base_postfix"]
+
+	   	exportFile := "output.xml"
+	   	stringToFile(exportFile, xml)
+	   	format(exportFile)
+	*/
 }
 
-// Converts a Node tree to XML string
-func nodeToXML(n Node) string {
-	xml := fmt.Sprintf("<%s>%s", n.Type, n.Label)
+func nodeToXML(n Node, filesMap map[string]string) string {
+
+	nodeType := n.Type
+
+	prefixContent := filesMap[nodeType+"_prefix"]
+	postfixContent := filesMap[nodeType+"_postfix"]
+
+	xml := prefixContent
+
 	for _, child := range n.Children {
-		xml += nodeToXML(child)
+		xml += nodeToXML(child, filesMap)
 	}
-	xml += fmt.Sprintf("</%s>", n.Type)
+
+	xml += postfixContent
+
 	return xml
 }
 
 func Generatexdomea(w http.ResponseWriter, r *http.Request) {
 
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	filesMap := readFiles()
 
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-    if r.Method == http.MethodOptions {
-        w.WriteHeader(http.StatusOK)
-        return
-    }
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 
 	if r.Method != http.MethodPost {
 		http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
@@ -54,16 +113,63 @@ func Generatexdomea(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	var nodes []Node
-	if err := json.Unmarshal(body, &nodes); err != nil {
-		http.Error(w, "Failed to parse JSON: "+err.Error(), http.StatusBadRequest)
-		return
+	if err := json.Unmarshal([]byte(body), &nodes); err != nil {
+		panic("json could not be parsed")
 	}
 
-	xml := ""
+	xml := filesMap["base_prefix"]
+
 	for _, node := range nodes {
-		xml += nodeToXML(node)
+		xml += nodeToXML(node, filesMap)
 	}
+
+	xml += filesMap["base_postfix"]
+
+	exportFile := "output.xml"
+	stringToFile(exportFile, xml)
+	format(exportFile)
 
 	w.Header().Set("Content-Type", "application/xml")
-	w.Write([]byte(xml))
+	w.Write([]byte(readFile("output.xml")))
+}
+
+func readFiles() map[string]string {
+	filesMap := make(map[string]string)
+	entries, err := os.ReadDir("./template")
+	if err != nil {
+		panic(err)
+	}
+	for _, e := range entries {
+		path := "template/"
+		fileName := e.Name()
+
+		fileContent := readFile(path + fileName)
+
+		filesMap[getKeyFromFileName(fileName)] = fileContent
+	}
+	return filesMap
+}
+
+func getKeyFromFileName(fileName string) string {
+	return strings.Split(fileName, ".")[0]
+}
+
+func stringToFile(path string, content string) error {
+	return os.WriteFile(path, []byte(content), 0644)
+}
+
+func format(fileName string) {
+	cmd := exec.Command("xmllint", "--format", fileName, "-o", fileName)
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile(path string) string {
+	fileContent, error := os.ReadFile(path)
+	if error != nil {
+		panic("file not found")
+	}
+	return string(fileContent)
 }
